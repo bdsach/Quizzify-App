d2617eb4818fe41c65593310d51b47abf95e6a27
Bandit Silachai <mbandit001@yahoo.co.th>
2024-01-04 11:33:35 +0700

    Initial Commit

---
+++ Quizzify/QuizzifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizzifyApp.swift
+//  Quizzify
+//
+//  Created by Bandit Silachai on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct QuizzifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
